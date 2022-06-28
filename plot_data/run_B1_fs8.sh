0a324ee1c46952d3db708c10769f99070603045b
manu <rescue11080@gmail.com>
2022-06-28 13:53:01 +0530

    updating plots as well

--- plot_data/run_B1_fs8.sh
+++ plot_data/run_B1_fs8.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-g++ ../delta_gen_friedmann_Cs.cpp -lm
-./a.out bimetric 0.3 0.0
-./a.out bimetric 0.3 0.5
-./a.out bimetric 0.3 1.0
-./a.out bimetric 0.3 3.0
-./a.out bimetric 0.3 6.0
+g++ ../code/delta_gen_friedmann_Cs_woD.cpp -lm
+#./a.out bimetric 0.3 0.0
+./a.out bimetric 0.3 0.2
+#./a.out bimetric 0.3 0.5
+#./a.out bimetric 0.3 1.0
+./a.out bimetric 0.3 2.0
+#./a.out bimetric 0.3 3.0
+#./a.out bimetric 0.3 6.0
 
 
